d8988e975c0f7e1ae82f5390bfbe9390cd1af0dd
Brandon Portheine <bportheine@gmail.com>
2018-03-14 10:50:12 +0100

    refactoring game logic

--- hangman-react-redux/src/lib/game.js
+++ hangman-react-redux/src/lib/game.js
@@ -1,13 +1,13 @@
-export const showGuess = (word, guesses) => {
-  return word.split('').map(letter => (guesses.indexOf(letter) < 0) ? "_" : letter).join(" ");
-}
-
 export const wrongGuessCount = (word, guesses) => {
   return guesses.filter(guess => word.indexOf(guess) < 0).length
 }
 
 export const wrongGuessLimit = (word, guesses) => {
-  return guesses.filter(guess => word.indexOf(guess) < 0).length >= 6
+  return wrongGuessCount(word, guesses) >= 6
+}
+
+export const showGuess = (word, guesses) => {
+  return word.split('').map(letter => (guesses.indexOf(letter) < 0) ? "_" : letter).join(" ");
 }
 
 export const isWinner = (word, guesses) => {
